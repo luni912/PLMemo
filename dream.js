016d6a5b05658129dea33f96d51bc630337e60bd
luni912 <luni912huang@gmail.com>
2025-07-28 07:51:44 +0000

    修改按鈕
    修改按鈕

--- dream.js
+++ dream.js
@@ -117,7 +117,7 @@ function updateNoteContent(list, data, type) {
 
     // 儲存按鈕
     const saveBtn = document.createElement('button');
-    saveBtn.textContent = 'V';
+    saveBtn.textContent = 'v';
     saveBtn.title = '儲存';
     saveBtn.addEventListener('click', () => {
       set(ref(db, `dream/${user}/${type}/${id}`), { text: inputText.value, date: inputDate.value });
